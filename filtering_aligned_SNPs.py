20122eb724aaef9a0127dfba004b19b257c8ed71
Abdelmonsif <39737270+Abdelmonsif@users.noreply.github.com>
2019-08-21 17:36:41 -0500

    Update filtering_aligned_SNPs.py

--- filtering_aligned_SNPs.py
+++ filtering_aligned_SNPs.py
@@ -1,16 +1,17 @@
 import pandas as pd
 import argparse
+import time
 
 def getArgs():
     parser = argparse.ArgumentParser('python')
-    parser.add_argument('-input',default= '/home/monsif/Downloads/Mapping_core-master/pdb_aligning_results',required=True,help='aligned SNPs input file')
+    parser.add_argument('-inputcsv',default= '/home/monsif/Downloads/Mapping_core-master/pdb_aligning_results',required=True,help='aligned SNPs input file')
     parser.add_argument('-output',default= '/home/monsif/Downloads/Mapping_core-master/final_aligning',required=True,help='output of filtering')
     parser.add_argument('-highest',default= '/home/monsif/Downloads/Mapping_core-master/highest_pdb',required=True,help='highest pdb after filteration')
     return parser.parse_args()
 
 
-def filtering(input,output,highest):
-    df = pd.read_csv(input, delim_whitespace=True, names=['protein', 'SNP_ID', 'wt_codon', 'mu_codon', 'Wild_type', 'Mutant', 'Position', 'Disorder', 'Condifdence', 'pdb', 'PDB_position' , 'ID'])
+def filtering(inputcsv,output,highest):
+    df = pd.read_csv(inputcsv, delim_whitespace=True, names=['protein', 'SNP_ID', 'wt_codon', 'mu_codon', 'Wild_type', 'Mutant', 'Position', 'Disorder', 'Condifdence', 'pdb', 'PDB_position' , 'ID'])
     df1 = df[df.ID != '-']
     df1.to_csv(output, index=False, sep='\t')
     df2 = df1.groupby(['protein', 'SNP_ID'], sort=False, as_index=False).first()
@@ -18,7 +19,7 @@ def filtering(input,output,highest):
 
 if __name__ == "__main__":
     args = getArgs()
-    filtering = filtering(args.input,args.output,args.highest)
+    filtering = filtering(args.inputcsv,args.output,args.highest)
     #input = 
     
     
